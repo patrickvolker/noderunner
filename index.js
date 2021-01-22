4a33e8a6d04286a08e242037b00cfb1a96e54ba4
Patrick Volker <pat@patrickvolker.com>
2021-01-22 11:53:08 -0500

    hello noderunner

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/stats', (req, res) => {
     });
 });
 
-app.listen(5001, () => console.log('Online!'));
+app.listen((process.env.dbPort || 5001), () => console.log('Online!'));
 
 
 //sql line for inserting new run ---> INSERT INTO `stats` (`id`, `run_date`, `run_length`, `run_total`) VALUES (NULL, '2021-01-08', '6', '48');
